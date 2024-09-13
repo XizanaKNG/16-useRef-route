bcafceae6770fba9180c10b5442753b8d9948e09
XizanaKNG <xizanakng@gmail.com>
2024-09-13 20:14:00 +0400

    useRef/example1

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useRef } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // let normalVar = 0; // ჩვეულებრივი ცვლადი
+
+  const [click, setClick] = useState(0);
+
+  const refVar = useRef(0);
+
+  console.log(refVar);
+
+  const handleClick = () => {
+    // normalVar += 1; // გავზარდოთ ნორმალური ცვლადი
+    refVar.current += 1;
+    // setClick(click + 1);
+    console.log("use Ref : ", refVar.current);
+  };
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <p>Button Clicked {click} Times</p>
+        <p>Var Variable: {refVar.current}</p>
+        <button onClick={handleClick}>Click me</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
